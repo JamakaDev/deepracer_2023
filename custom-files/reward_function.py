ebd6ef0af64a5c05ccd38bec9e5e6ce0357931ab
Jon Adams <jamakadev@gmail.com>
2023-07-04 08:54:30 -0400

    fixed indentation errors

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -56,21 +56,12 @@ def reward_function(params):
         if (-5.0 <= steering_angle <= 5.0) and (speed >= SPEEDING_THRESHOLD):
             reward *= 2
 
-	marker_1 = 0.1 * track_width
-        marker_2 = 0.25 * track_width
-        marker_3 = 0.5 * track_width
-        
         # Give higher reward if the car is closer to center line and vice versa
-        if distance_from_center <= marker_1:
-            reward *= 2
-        elif distance_from_center <= marker_2:
-            reward *= 1.25
-        elif distance_from_center <= marker_3:
-            reward *= 1.1
-        else:
-            reward = 1e-3
-
-        
+        if distance_from_center   <= 0.1 * track_width: reward *= 2
+        elif distance_from_center <= 0.25 * track_width: reward *= 1.25
+        elif distance_from_center <= 0.5 * track_width: reward *= 1.1
+        else: reward = 1e-3
+       
     
     return float(reward)
 
