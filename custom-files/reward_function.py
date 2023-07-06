febedcb2bcdc846d0eabd2579d0ea508e2f16208
Jon Adams <jamakadev@gmail.com>
2023-07-05 22:57:29 -0400

    updated reward func to run jon-v2

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,7 +1,10 @@
+import math
+
 def reward_function(params):
     
     all_wheels_on_track = params['all_wheels_on_track']
     car_direction = params['heading']
+    car_is_offtrack = params['is_offtrack']
     distance_from_center = params['distance_from_center']
     progress = params['progress']
     speed = params['speed']
@@ -11,35 +14,45 @@ def reward_function(params):
     waypoints = params['waypoints']
     x_coord = params['x']
     y_coord = params['y']
-    
-    # Max steps b4 episode is terminated (avoids running indefinitely)
-    MAX_STEPS = 300 
-    progress_factor = 1.15
-    
-    # Attempting to reward higher speeds
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
     reward = speed
+
+    DIRECTION_THRESHOLD = 10.0
+    MAX_STEPS = 300 
+    PROGRESS_FACTOR = 1.15
     
     # Penalize if the car goes off-track
-    if not all_wheels_on_track and speed < 2:
-        reward = 1e-3
+    if not all_wheels_on_track and car_is_offtrack:
+        return 1e-3
+
+    if distance_from_center <= marker_1:
+        reward *= 2
+    elif distance_from_center <= marker_2:
+        reward *= 1.5
+    elif distance_from_center <= marker_3:
+        reward *= 1.25
     else:
-        # Get direction of next waypoint
-        next_index = int((progress / 100) * (len(waypoints) - 1))
-        next_coord = waypoints[next_index]
-        track_direction = next_coord[0] - x_coord, next_coord[1] - y_coord
-        
-        # Get angle between the car direction and the track direction
-        direction_diff_x = abs(track_direction[0] - car_direction)
-        direction_diff_y = abs(track_direction[1] - car_direction)
-        
-        # Penalize if the car deviates from the track direction
-        if direction_diff_x > 1.0 or direction_diff_y > 1.0:
-            reward *= 0.5
-        
-        # Reward if the car is closer to the center of the track
-        reward += (1 - (distance_from_center / (track_width / 2))) * 0.15
-        
-        # Reward additional progress
-        reward += (progress - (steps / MAX_STEPS)) * progress_factor
+        return 1e-3
+
+    # Get direction of next waypoint
+    next_point = waypoints[params['closest_waypoints'][1]]
+    track_direction = math.atan2(next_point[0] - x_coord, next_point[1] - y_coord)
+    track_direction = math.degrees(track_direction)
+    
+    # Get angle between the car direction and the track direction
+    direction_diff = abs(track_direction - car_direction)
+    if direction_diff > 180: direction_diff = 360 - direction_diff
+    
+    # Penalize if the diff too large
+    if direction_diff > DIRECTION_THRESHOLD: reward *= 0.75
+    else: reward *= 1.25
+    
+    # Reward if the car is closer to the center of the track
+    reward += (1 - (distance_from_center / (track_width / 2))) * 0.15
+    
+    # Reward additional progress
+    reward += (progress - (steps / MAX_STEPS)) * PROGRESS_FACTOR
     
     return float(reward)
