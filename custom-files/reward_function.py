0f1cf36184be3f5f2bcc17949d6014fe3dfe4870
Jon Adams <jamakadev@gmail.com>
2023-07-13 16:46:51 -0400

    jon-v1 7/13/23

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -22,38 +22,27 @@ def reward_function(params):
     DIRECTION_THRESHOLD = 10.0
     MAX_STEPS = 300 
     PROGRESS_FACTOR = 1.15
-    SPEED_THRESHOLD = 2.5
+    SPEED_THRESHOLD = 2.25
     
-    # Penalize if the car goes off-track
-    if not all_wheels_on_track and car_is_offtrack:
-        return 1e-3
+    if not all_wheels_on_track and car_is_offtrack: return 1e-3
 
-    if distance_from_center <= marker_1:
-        reward *= 2.5
-    elif distance_from_center <= marker_2:
-        reward *= 1.75
-    elif distance_from_center <= marker_3:
-        reward *= 1.25
-    else:
-        return 1e-3
+    if distance_from_center <= marker_1: reward *= 2.5
+    elif distance_from_center <= marker_2: reward *= 1.75
+    elif distance_from_center <= marker_3: reward *= 1.25
+    else: return 1e-3
 
-    # Get direction of next waypoint
     next_point = waypoints[params['closest_waypoints'][1]]
     track_direction = math.atan2(next_point[0] - x_coord, next_point[1] - y_coord)
     track_direction = math.degrees(track_direction)
     
-    # Get angle between the car direction and the track direction
     direction_diff = abs(track_direction - car_direction)
     if direction_diff > 180: direction_diff = 360 - direction_diff
     
-    # Penalize if the diff too large
+
     if direction_diff > DIRECTION_THRESHOLD: reward *= 0.5
     else: reward *= 1.25
     
-    # Reward if the car is closer to the center of the track
-    reward += (1 - (distance_from_center / (track_width / 2))) * 0.25
-    
-    # Reward additional progress
+    reward += (1 - (distance_from_center / (track_width / 2))) * 0.25    
     reward += (progress - (steps / MAX_STEPS)) * PROGRESS_FACTOR
     
     if speed > SPEED_THRESHOLD: reward *= 3.0
