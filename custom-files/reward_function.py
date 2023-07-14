194bdec734a836fc709b56ed03ad463f979ddda0
Jon Adams <jamakadev@gmail.com>
2023-07-13 22:30:33 -0400

    jon-v1 -> jon-x1 speed only

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,4 +1,10 @@
 def reward_function(params):
-    # Penalize if the car goes off-track
-    if not params['all_wheels_on_track'] or params['is_offtrack']: return 1e-4
-    else: return float(params['speed'])
+    
+    all_wheels_on_track = params['all_wheels_on_track']
+    car_is_offtrack = params['is_offtrack']
+    speed = params['speed']
+    next_step = params['waypoints'][params['closest_waypoints'][1]]
+
+    if not all_wheels_on_track or car_is_offtrack: return 1e-4
+    if next_step in set(range(20, 40)) and speed > 2: return 1e-3
+    return float(speed)
