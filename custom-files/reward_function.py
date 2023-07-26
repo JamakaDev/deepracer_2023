7d2633b1c4acb1d0ec8017eece897d2f6222878b
Jon Adams <jamakadev@gmail.com>
2023-07-26 16:04:14 -0400

    jon-v1 7/26

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -8,6 +8,7 @@ def reward_function(params):
     steering_angle = params['steering_angle']
     steps = params['steps']
     track_width = params['track_width']
+    is_reversed = params['is_reversed']
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width 
@@ -17,7 +18,7 @@ def reward_function(params):
     STEPS_THRESHOLD = 250 
     PROGRESS_FACTOR = 1.25
 
-    if not all_wheels_on_track or car_off_track: return 1e-3
+    if not all_wheels_on_track or car_off_track or is_reversed: return 1e-3
 
     if distance_from_center <= marker_1: reward = reward * 3 if speed >= SPEEDING_THRESHOLD else reward * 2
     elif distance_from_center <= marker_2: reward *= 1.5
