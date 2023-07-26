f58a4f6c7a898de3bb1eecab4dfd8a4f97770de2
Jon Adams <jamakadev@gmail.com>
2023-07-26 16:06:09 -0400

    jon-v1s 7/26

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -4,7 +4,8 @@ def reward_function(params):
     car_is_offtrack = params['is_offtrack']
     speed = params['speed']
     next_step = params['closest_waypoints'][1]
+    is_reversed = params['is_reversed']
 
-    if not all_wheels_on_track or car_is_offtrack: return 1e-4
+    if not all_wheels_on_track or car_is_offtrack or is_reversed: return 1e-4
     if next_step in list(range(20, 40)) and speed > 2: return 1e-3
     return float(speed)
