0000292341c6183192c55edbbe59e3029219584d
Jon Adams <jamakadev@gmail.com>
2023-07-26 16:01:35 -0400

    jon-x2s 7/26

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -5,11 +5,12 @@ def reward_function(params):
     reward = speed = params['speed']
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
+    is_reversed = params['is_reversed']
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
     
-    if not all_wheels_on_track or car_is_offtrack: return 1e-4
+    if not all_wheels_on_track or car_is_offtrack or is_reversed: return 1e-4
     if   distance_from_center <= marker_1: reward += 2
     elif distance_from_center <= marker_2: reward += 1.5
     elif distance_from_center <= marker_3: reward += 1.25
