e6d67bb283757f4d09fada74a8b8d1b19c48a1d8
Jon Adams <jamakadev@gmail.com>
2023-07-29 15:33:47 -0400

    jon-v3s 7/29

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -27,6 +27,12 @@ def reward_function(params):
     # Penalize if the car goes off-track
     if not all_wheels_on_track or car_off_track or is_reversed: return 1e-3
     
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1: reward = reward * 5 if speed >= SPEEDING_THRESHOLD else reward * 3
+    elif distance_from_center <= marker_2: reward = reward * 2.5 if speed >= SPEEDING_THRESHOLD else reward * 1.5
+    elif distance_from_center <= marker_3: reward = reward * 1.25 if speed >= SPEEDING_THRESHOLD else reward * 1.1
+    else: reward = 1e-3
+    
     # Get direction of next waypoint
     prev_waypoint = waypoints[closest_waypoints[0]]
     next_waypoint = waypoints[closest_waypoints[1]]
@@ -51,12 +57,6 @@ def reward_function(params):
     if (abs(steering_angle) <= 5.0) and (speed >= SPEEDING_THRESHOLD): reward *= 2
     
     if (marker_3 - distance_from_center) >= 0.1: reward *= 2
-    
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1: reward = reward * 3 if speed >= SPEEDING_THRESHOLD else reward * 2
-    elif distance_from_center <= marker_2: reward *= 1.5
-    elif distance_from_center <= marker_3: reward *= 1.1
-    else: reward = 1e-3
      
     if not next_waypoint in list(range(20,105)) and speed > 3: reward * 10
 
