2e7cdfd693987d76e873c6e9e03b76820426b424
Jon Adams <jamakadev@gmail.com>
2023-07-29 12:33:15 -0400

    jon-v2 7/29

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,62 +1,44 @@
-import math
 def reward_function(params):
-    
-    all_wheels_on_track = params['all_wheels_on_track']
-    car_direction = params['heading']
-    car_off_track = params['is_offtrack']
-    is_reversed = params['is_reversed']
-    closest_waypoints = params['closest_waypoints']
-    distance_from_center = params['distance_from_center']
-    progress = params['progress']
-    reward = speed = params['speed']
-    steering_angle = params['steering_angle']
-    steps = params['steps']
-    track_width = params['track_width']
-    waypoints = params['waypoints']
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
-    
-    
-    DIRECTION_THRESHOLD = 10.0
-    SPEEDING_THRESHOLD = 2.0
-    STEPS_THRESHOLD = 275 
-    PROGRESS_FACTOR = 1.25
-        
 
-    # Penalize if the car goes off-track
-    if not all_wheels_on_track or car_off_track or is_reversed: return 1e-3
-    
-    # Get direction of next waypoint
-    prev_waypoint = waypoints[closest_waypoints[0]]
-    next_waypoint = waypoints[closest_waypoints[1]]
+    center_variance = params["distance_from_center"] / params["track_width"]
 
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
-    track_direction = math.degrees(math.atan2(next_waypoint[1] - prev_waypoint[1], next_waypoint[0] - prev_waypoint[0]))
+    left_lane = [
+        24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,66,67,
+        68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88
+    ]
+    center_lane = [
+        0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,44,45,
+        46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,89,90,91,
+        92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,
+        111,112,113,114,115,116,117,118
+    ]  
+    
+    fast = [
+        0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,46,47,
+        48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,
+        72,73,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,
+        100,101,102,103,104,105,106,107,108,109,110,111,112,113,117,118
+    ]
+    slow = [
+        25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45
+    ]
+    
+    reward = 42.0
 
-    # Calculate the difference between the track direction and the heading direction of the car
-    direction_diff = abs(track_direction - car_direction)
-    if direction_diff > 180: direction_diff -= 360
+    if params["all_wheels_on_track"]:
+        reward += 10.0
+    else:
+        reward -= 10.0
 
-    # Penalize the reward if the difference is too large
-    if direction_diff > DIRECTION_THRESHOLD: reward *= 0.5
-    else: reward *= 1.25
+    if params["closest_waypoints"][1] in left_lane and params["is_left_of_center"]: reward += 10.0
+    elif params["closest_waypoints"][1] in center_lane and center_variance < 0.5: reward += 10.0
+    else: reward -= 10.0
     
-    # Reward if the car is closer to the center of the track
-    reward += (1 - (distance_from_center / (track_width / 2))) * 0.2
-    
-    # Reward additional progress
-    reward += (progress - (steps / STEPS_THRESHOLD)) * PROGRESS_FACTOR
-
-    if (abs(steering_angle) <= 5.0) and (speed >= SPEEDING_THRESHOLD): reward *= 2
-    
-    if (marker_3 - distance_from_center) >= 0.1: reward *= 2
-    
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1: reward = reward * 3 if speed >= SPEEDING_THRESHOLD else reward * 2
-    elif distance_from_center <= marker_2: reward *= 1.5
-    elif distance_from_center <= marker_3: reward *= 1.1
-    else: reward = 1e-3
-     
-    
+    if params["closest_waypoints"][1] in fast:
+        if params["speed"] >= 2 : reward += 10.0
+        else: reward -= 10.0
+    elif params["closest_waypoints"][1] in slow:
+        if params["speed"] <= 2 : reward += 10.0
+        else: reward -= 10.0
+            
     return float(reward)
